cceae1b6bf8ad72ebb31a20eb1ea03b96af82589
火星小刘 <xtlyk@126.com>
2022-08-22 19:04:11 +0800

    更新教程

--- oAuth/admin.py
+++ oAuth/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext, gettext_lazy as _
-from oAuth.models import NewUser
+from oAuth.models import NewUser, Student, StudentInfo, ClassName, Teacher
 
 # Register your models here.
 
@@ -19,3 +19,27 @@ class NewUserAdmin(UserAdmin):
     search_fields = ('username', 'email')
 
 admin.site.register(NewUser, NewUserAdmin)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'student_info', 'class_name', 'is_finished')
+    list_display_links = ('id', 'name', 'student_info', 'class_name', 'is_finished')
+
+admin.site.register(Student, StudentAdmin)
+
+class StudentInfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'address')
+    list_display_links = ('id', 'address')
+
+admin.site.register(StudentInfo, StudentInfoAdmin)
+
+class ClassNameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+admin.site.register(ClassName, ClassNameAdmin)
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+admin.site.register(Teacher, TeacherAdmin)
--- oAuth/models.py
+++ oAuth/models.py
@@ -17,6 +17,7 @@ class NewUser(AbstractUser):
     last_login = models.DateTimeField(_('last login'), blank=True, null=True, auto_now=True)
     code = models.UUIDField(verbose_name='uuid', default=uuid.uuid4, editable=False)
 
+
     objects = UserManager()
 
     class Meta(AbstractUser.Meta):
@@ -28,3 +29,46 @@ class Books(models.Model):
     name = models.CharField(verbose_name='书名', max_length=10)
     auther = models.CharField(verbose_name='作者', max_length=10)
     is_delete = models.BooleanField(verbose_name='是否删除', default=False)
+
+    class Meta:
+        verbose_name_plural = '图书'
+
+class Student(models.Model):
+    name = models.CharField(verbose_name='姓名', max_length=10)
+    is_finished = models.BooleanField(verbose_name='是否毕业', default=False)
+    student_info = models.OneToOneField(verbose_name='学生信息', to='StudentInfo', on_delete=models.CASCADE, null=True, blank=True)
+    class_name = models.ForeignKey(verbose_name='班级', to='ClassName', on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "学生"
+
+    def __str__(self):
+        return  str(self.id) + '--' + self.name
+
+class StudentInfo(models.Model):
+    address = models.CharField(verbose_name='地址', max_length=10)
+
+    class Meta:
+        verbose_name_plural = "学生信息"
+
+    def __str__(self):
+        return str(self.id) + '--' + self.address
+
+class ClassName(models.Model):
+    name = models.CharField(verbose_name='班级名称', max_length=10)
+
+    class Meta:
+        verbose_name_plural = '班级'
+
+    def __str__(self):
+        return  str(self.id) + '--' + self.name
+
+class Teacher(models.Model):
+    name = models.CharField(verbose_name='姓名', max_length=10)
+    student = models.ManyToManyField(verbose_name='学生', to='Student', blank=True)
+
+    class Meta:
+        verbose_name_plural = '老师'
+
+    def __str__(self):
+        return  str(self.id) + '--' + self.name
